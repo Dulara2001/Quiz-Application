f065903809c7dab9ea6bc0d20e088db74e12eaa8
dulara <it21014840@my.sliit.lk>
2023-01-07 16:41:51 +0530

    final commit

--- quiz.js
+++ quiz.js
@@ -43,49 +43,73 @@ let wrongCount = 0;
 let total = 0;
 let selectedAnswer;
 
-
-const showResult = () => {
-  
-}
-
-
-const showQuetion = (qNumber) => {
-  selectedAnswer = null;
-  question.textContent = data[qNumber].question
-  answersContainer.innerHTML = data[qNumber].answers.map((item,index) => 
-    `
-    <div class="answer">
-      <input type="radio" id=${index} name="answer" value=${item.isCorrect}/>
-      <label for="1">${item.answer}</label>
-     </div>
-     `
-    
-  ).join("");
-
-  selectAnswer()
+const playAgain = () => {
+  qIndex = 0;
+  correctCount = 0;
+  wrongCount = 0;
+  total = 0;
+  showQuestion(qIndex);
 };
 
-const selectAnswer = ()=> {
-  answersContainer.querySelectorAll("input").forEach(el=>{
-    el.addEventListener("click",(e)=>{
+play.addEventListener("click",()=>{
+  resultScreen.style.display = "none";
+  gameScreen.style.display = "block";
+  playAgain()
+})
+
+const showResult = () => {
+  resultScreen.style.display = "block";
+  gameScreen.style.display = "none";
+
+  resultScreen.querySelector(
+    ".correct"
+  ).textContent = `Correct Answers: ${correctCount}`;
+
+  resultScreen.querySelector(
+    ".wrong"
+  ).textContent = `Wrong Answers: ${wrongCount}`;
+
+  resultScreen.querySelector(".score").textContent = `Score: ${
+    (correctCount - wrongCount) * 10
+  }`;
+};
+
+const showQuestion = (qNumber) => {
+  if (qIndex === data.length) return showResult();
+  selectedAnswer = null;
+  question.textContent = data[qNumber].question;
+  answersContainer.innerHTML = data[qNumber].answers
+    .map(
+      (item, index) =>
+        `
+  <div class="answer">
+      <input type="radio" id=${index} name="answer" value=${item.isCorrect} />
+      <label for="1">${item.answer}</label>
+  </div>
+  `
+    )
+    .join("");
+
+  selectAnswer();
+};
+
+const selectAnswer = () => {
+  answersContainer.querySelectorAll("input").forEach((el) => {
+    el.addEventListener("click", (e) => {
       selectedAnswer = e.target.value;
     });
-    
   });
 };
 
+const submitAnswer = () => {
+  submit.addEventListener("click", () => {
+    if (selectedAnswer !== null) {
+      selectedAnswer === "true" ? correctCount++ : wrongCount++;
+      qIndex++;
+      showQuestion(qIndex);
+    } else alert("Select an answer!");
+  });
+};
 
-const submitAnswer =  ()=> {
-  submit.addEventListener("click",()=> {
-    if(selectedAnswer !== null){
-      selectedAnswer === "true" ? correctCount++ : wrongCount++
-      qIndex++
-      showQuetion(qIndex)
-    }else 
-      alert("select an answer");
-  })
-}
-
-
-showQuetion(qIndex);
+showQuestion(qIndex);
 submitAnswer();
